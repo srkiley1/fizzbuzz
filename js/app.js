d8f87efd19098de2c5845fc8620c6432e3590ef8
srkiley1 <srk2@att.net>
2015-07-25 09:07:26 -0700

    appended

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-
-	for (i=1; i<=100; i++){
-	console.log(i);
-
-	}
-	if (i % 15 === 0){
-		console.log("FizzBuzz");
-	}
-	else if (i % 3 === 0){
-		console.log("Fizz");
-	}
-	else if(i % 5=== 0){
-	console.log("Buzz");
-	}
-	else {
-	console.log(i);
-	}
-
+	var i =i<=20
+	for (i=1; i<=20; i++){
+	
+		if (i % 15 === 0){
+			$("#append2").append("<li>FizzBuzz</li>");
+			}
+		else if (i % 3 === 0){
+			$("#append2").append("<li>Fizz</li>");
+			}
+		
+		else if(i % 5=== 0){
+		$("#append2").append("<li>Buzz</li>");
+			}
+		else {
+		$("#append2").append("<li>i</li>");
+			}
 			
+	}
 });
